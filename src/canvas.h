5958f55300d8489fe38e6f7e144b9cd08a24b69b
Ohad <Ohad@laptop>
2018-01-18 20:15:41 +0200

    control checkpoint

--- src/canvas.h
+++ src/canvas.h
@@ -9,7 +9,7 @@
 #include <QMatrix4x4>
 #include <QMouseEvent>
 #include <QDebug>
-
+#include <iostream>
 #include <cmath>
 
 #include "backdrop.h"
@@ -68,7 +68,7 @@ public:
 		m_lines[0].setColor(0, 1, 0, 1);
 		m_lines[0].setLineWidth(1);
 
-
+		privSetTrackBallCircles();
 
 
 	}
@@ -91,6 +91,9 @@ public:
 			m_lines[i].draw(privTmat(), privVmat());
 
 
+		m_trackBall[0].draw(privTmat(), privVmat());
+		m_trackBall[1].draw(privTmat(), privVmat());
+		m_trackBall[2].draw(privTmat(), privVmat());
 
 
 
@@ -151,11 +154,10 @@ public:
 			QVector3D lower(m_meshDataP->xmin(), m_meshDataP->ymin(), m_meshDataP->zmin());
 			QVector3D upper(m_meshDataP->xmax(), m_meshDataP->ymax(), m_meshDataP->zmax());
 			m_center = (lower + upper) / 2;
-			m_eye = m_center + QVector3D(0, -m_meshDataP->zmax()*2, 0);
-			m_upvec = QVector3D(0, 0, 1);
-			//m_scale = 1;
-
-			// Reset other camera parameters
+			//TODO : use privFOV to calculate init distance
+			m_eye = m_center + QVector3D(0, 0,m_meshDataP->ymin()*4);
+			m_upvec = QVector3D(0, 1, 0);
+		
 
 
 		}
@@ -168,11 +170,38 @@ public:
 
 protected:
 
+	void privSetTrackBallCircles()
+	{
+		
+		m_trackBall[0].setColor(.92, .14, .14);
+		m_trackBall[1].setColor(.14, .92, .14);
+		m_trackBall[2].setColor(.14, .14, .92);
+		m_trackBall[0].setLineWidth(1);
+		m_trackBall[1].setLineWidth(1);
+		m_trackBall[2].setLineWidth(1);
+
+		QVector3D cntrlRay = privMousePos2ray(QPoint(width() / 2, height() / 2));
+		QVector3D outrlRay = privMousePos2ray(QPoint(width() / 2+privTBradius(), height() / 2));
+		float r = (outrlRay - cntrlRay).length()*(m_center - m_eye).length();
+		QMatrix4x4 t;
+		
+		t.translate(m_center);
+		t.scale(r);
+		
+		m_trackBall[0].set(t);
+		t.rotate(90, 0, 1, 0);
+		m_trackBall[1].set(t);
+		t.rotate(90, 1, 0, 0);
+		m_trackBall[2].set(t);
+
+	}
+
+
 	void mouseDoubleClickEvent(QMouseEvent* event)
 	{
-		QVector3D r = privMousePos2ray(m_mousePos);
+	
 
-		std::array<QVector3D, 2> p = m_meshDataP->closest2ray(m_eye, r);
+		std::array<QVector3D, 2> p = m_meshDataP->closest2ray(m_eye, m_mouseRay);
 		m_center = p[0];
 		update();
 
@@ -184,9 +213,9 @@ protected:
 		if (event->key() == Qt::Key_C)
 		{
 
-			QVector3D r = privMousePos2ray(m_mousePos);
+			
 
-			std::array<QVector3D, 2> p = m_meshDataP->closest2ray(m_eye, r);
+			std::array<QVector3D, 2> p = m_meshDataP->closest2ray(m_eye, m_mouseRay);
 
 
 			QMatrix4x4 t;
@@ -210,9 +239,18 @@ protected:
 		if (event->button() == Qt::LeftButton ||
 			event->button() == Qt::RightButton)
 		{
-			m_mousePos = event->pos();
+			
 			setCursor(Qt::ClosedHandCursor);
+			
 		}
+		m_mousePressPos = event->pos();
+		m_mousePressEye = m_eye;
+		m_mousePressUpvec = m_upvec;
+		
+		
+	
+
+
 	}
 
 	void mouseReleaseEvent(QMouseEvent* event)
@@ -224,26 +262,92 @@ protected:
 		}
 	}
 
+	static float minmax1(float v)
+	{
+		return std::min(1.0f, std::max(-1.0f, v));
+	}
+	static QMatrix4x4 vvrotate( QVector3D v0,  QVector3D v1)
+	{
+		v0.normalize();
+		v1.normalize();
+		static const float rad2deg = 90.0 / std::acos(0);
+		QVector3D ra = -QVector3D::crossProduct(v0, v1);
+		float rd = std::acos(QVector3D::dotProduct(v0, v1))*rad2deg;
+		QMatrix4x4 m;
+		m.rotate(rd, ra);
+		return m;
+	}
 	void mouseMoveEvent(QMouseEvent* event)
 	{
-		auto oldPos = event->pos();
-		auto d = oldPos - m_mousePos;
-		m_mousePos = oldPos;
-
-		QVector3D rayO = privMousePos2ray(oldPos);
-		QVector3D rayO = privMousePos2ray(m_mousePos);
+		static const float rad2deg = 90.0 / std::acos(0);
+		QPoint newPos = event->pos();
+		QPoint d = newPos - m_mousePos;
 
 
+		
+		
+
+		QVector3D ray0 = privMousePos2ray(newPos);
+		QVector3D ray1 = m_mouseRay;
+		QVector3D n = m_mousePressEye - m_center;
+		
+		
+		float tbRadius = privTBradius();
+		float tbRadius2 = tbRadius2*tbRadius2;
+		QPoint c(width()/2, height()/2);
 		if (event->buttons() & Qt::LeftButton)
 		{
-			QVector3D n = m_eye - m_center;
-			float yaw = fmod( -d.x(), 360);
-			float tilt = fmod( -d.y(), 360);
-			QMatrix4x4 m;
-			m.rotate(tilt, QVector3D::crossProduct(m_upvec,n));
-			m.rotate(yaw, m_upvec);
-			n = m*n;
-			m_eye = m_center + n;
+			
+			QVector3D n0(minmax1((m_mousePressPos.x() - width() / 2) / tbRadius), minmax1((m_mousePressPos.y() - height() / 2) / tbRadius), 0);
+			QVector3D n1(minmax1((m_mousePos.x()      - width() / 2) / tbRadius), minmax1((m_mousePos.y()      - height() / 2) / tbRadius), 0);
+			n0.setZ(-std::sqrt(std::max(0.0f, 1 - n0.x()*n0.x() - n0.y()*n0.y())));
+			n1.setZ(-std::sqrt(std::max(0.0f, 1 - n1.x()*n1.x() - n1.y()*n1.y())));
+			QMatrix4x4 m2 = vvrotate(n, QVector3D(0, 0, -1));
+
+			n0 = m2*n0;
+			n1 = m2*n1;
+
+			QMatrix4x4 m = vvrotate(n0,n1);
+			
+		
+			QVector3D n = m_mousePressEye - m_center;
+
+			m_eye = m*(n)+ m_center;
+			m_upvec = m*m_mousePressUpvec;
+			
+			QVector3D nn = n;
+			nn.normalize();
+			std::cout << "nn:" << nn.x() << "," << nn.y() << "," << nn.z() ;
+			std::cout << "n0:" << n0.x() << "," << n0.y() << "," << n0.z();
+			std::cout << std::endl;
+
+			//QPoint od = (c - m_mousePressPos);
+			//float d2 = od.x()*od.x() + od.y()*od.y();
+			//if (d2 < tbRadius2)
+			//{
+			//	QMatrix4x4 m;
+			//	m.rotate(QVector3D::dotProduct(ray0, ray1), -QVector3D::crossProduct(ray0, ray1));
+			//	n = m*n;
+			//	m_eye = m_center + n;
+			//	m_upvec = m*m_upvec;
+			//}
+			//else
+			//{
+			//	static const float rad2deg = 90.0/std::acos(0) ;
+			//	QVector2D va(newPos.x() - c.x(), newPos.y() - c.y());
+			//	QVector2D vb(m_mousePos.x() - c.x(), m_mousePos.y() - c.y());
+			//	va.normalize();
+			//	vb.normalize();
+
+			//	//float phi=std::acos(QVector2D::dotProduct(va, vb) );
+			//	float phi = (std::atan2f(va.y(), va.x()) - std::atan2f(vb.y(), vb.x()))*rad2deg;
+			//	QMatrix4x4 m;
+			//	m.rotate(phi, n);
+			//	m_upvec = m*m_upvec;
+			//	//std::cout << phi<<"   "<< m_upvec.x() << "," << m_upvec.y() << "," << m_upvec.z() << std::endl;
+			//}
+			////std::cout << "   " << od.x() << "," << od.y() <<  std::endl;
+			
 
 			update();
 		}
@@ -252,16 +356,18 @@ protected:
 			QVector3D n = m_eye - m_center;
 
 			QMatrix4x4 m;
-			m.translate(-d.x()*QVector3D::crossProduct(m_upvec, n));
-			m.translate(-d.y()*m_upvec);
+			QVector3D dv = (ray1 - ray0)*n.length();
+			m.translate(dv);
+	
 			m_center = m*m_center;
+			privSetTrackBallCircles();
 			m_eye = m*m_eye;
 			update();
+			//std::cout << dv.x()<<","<<dv.y()<<","<<dv.z() << std::endl;
 		}
 
-
-
-		
+		m_mousePos = newPos;
+		m_mouseRay =  privMousePos2ray(m_mousePos);
 	}
 
 	void wheelEvent(QWheelEvent *event)
@@ -272,7 +378,7 @@ protected:
 		auto n = m_eye- m_center;
 		n *= numDegrees.y() > 0 ? 9.0/10 : 10.0/9;
 		m_eye = m_center + n;
-		
+		privSetTrackBallCircles();
 		update();
 	}
 
@@ -307,6 +413,19 @@ private:
 			s = v;
 		return v;
 	}
+	static float privFOV(float s = 0)
+	{
+		float v = 45;
+		if (s != 0)
+			s = v;
+		return v;
+	}
+	float privTBradius()
+	{
+		float v = std::min(width(),height())/3;
+		return v;
+	}
+
 
 
 	//void DrawCircle(float cx, float cy, float cz, float r)
@@ -363,11 +482,9 @@ private:
 		QMatrix4x4 m;
 
 		float ratio = width() / (float)height();
-
-		//m.ortho(-ratio, ratio, -1.f, 1.f, 0.f, 10.f);
-		
-		//m.frustum(-ratio*S, ratio*S, -S, S, S, 100.f);
-		m.perspective(45, ratio, privZnear(), 10.f);
+		//m.ortho(-ratio, ratio, -1.f, 1.f, 0.f, 10000.f);
+		//m.frustum(-ratio*S, ratio*S, -S, S, S, 10000.f);
+		m.perspective(privFOV(), ratio, privZnear(), 10000.f);
 		
 
 
@@ -403,10 +520,15 @@ private:
 	QPropertyAnimation anim;
 
 	QPoint m_mousePos;
+	QVector3D m_mouseRay;
+	QPoint m_mousePressPos;
+	QVector3D m_mousePressEye;
+	QVector3D m_mousePressUpvec;
 	QString m_status;
 	std::vector<Circle3d> m_circles;
 	std::vector<Line3d> m_lines;
 
+	Circle3d m_trackBall[3];
 
 
 
